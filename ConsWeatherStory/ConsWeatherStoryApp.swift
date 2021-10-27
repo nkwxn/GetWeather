311dc7377933971315ebbc0476c6c92690087686
Nicholas <nichz123@gmail.com>
2021-10-27 15:16:42 +0700

    Initial Commit

---
+++ ConsWeatherStory/ConsWeatherStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsWeatherStoryApp.swift
+//  ConsWeatherStory
+//
+//  Created by Nicholas on 27/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ConsWeatherStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
